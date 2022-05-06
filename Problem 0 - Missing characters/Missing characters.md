9c07fe5af98bc3f9ef2345ff1a5feaaf2cda90d8
Marcelo Azevedo <marcellorhcp@gmail.com>
2022-05-05 22:40:52 -0300

    adding problem 0

---
+++ Problem 0 - Missing characters/Missing characters.md
@@ -0,0 +1,10 @@
+**Problem 0**
+
+Using your favorite language, make an algorithm that given a string with letters returns the missing character holes from A-Z
+
+Example:
+"abcdf" => "e"
+"acdf" => "be"
+"OQRS" => "P"
+"acdb" => ""
+"abcz" => "defghijklmnopqrstuvwxy"
---
+++ Problem 0 - Missing characters/main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"fmt"
+)
+
+func CheckLetters(letters string) []string {
+	var missingLetters = []string{}
+	var limit int
+	index := int(letters[0])
+
+	if int(letters[0]) > 64 && int(letters[0]) < 91 {
+		limit = 91
+	}
+	if int(letters[0]) > 96 && int(letters[0]) < 123 {
+		limit = 123
+	}
+
+	for i := 0; i < len(letters); i++ {
+		for j := index; j < limit; j++ {
+			if int(letters[i]) == j {
+				index += 1
+				break
+			} else {
+				missingLetters = append(missingLetters, string(j))
+				index += 1
+			}
+		}
+	}
+	return missingLetters
+}
+
+func main() {
+	fmt.Println(CheckLetters("abcdf"))
+	fmt.Println(CheckLetters("acdf"))
+	fmt.Println(CheckLetters("OQRS"))
+	// fmt.Println(CheckLetters("acdb"))
+	fmt.Println(CheckLetters("abcz"))
+}
